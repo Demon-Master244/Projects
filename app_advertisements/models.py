07a7055f1e0312906b29cb6c03cebb0f49ae275b
Demon-Master244 <abaiin2007@mail.ru>
2023-10-07 00:43:08 +0300

    Rabota 2.1

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -24,4 +24,10 @@ class Advertisement(models.Model):
             return format_html('<span style="color:green; font-weight:bold"> Сегодня в {}</span>', created_time)
         return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
-
+    @admin.display(description='дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:yellow; font-weight:bold"> Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
